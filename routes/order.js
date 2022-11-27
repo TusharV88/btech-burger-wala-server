b6011ac57017d7e41674eff1077e942eba19b281
TusharV88 <66867543+TusharV88@users.noreply.github.com>
2022-11-27 21:14:41 +0530

    Add Headers

--- routes/order.js
+++ routes/order.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import { getAdminOrders, getOrderDetails, getOrders, paymentVerification, placeOrder, processOrders } from '../controllers/order.js';
 import { authorizeAdmin, isAuthenticated } from '../middleware/auth.js';
+import { routerHeaders } from './user.js';
 
 const router = express.Router();
 
-router.post("/placeorder", isAuthenticated, placeOrder);
-router.post("/placeorderonline", isAuthenticated, placeOrder);
-router.post("/paymentverification", isAuthenticated, paymentVerification);
-router.get("/myorders", isAuthenticated, getOrders);
-router.get("/order/:id", isAuthenticated, getOrderDetails);
-router.get("/admin/orders", isAuthenticated, authorizeAdmin, getAdminOrders);
-router.get("/admin/order/:id", isAuthenticated, authorizeAdmin, processOrders);
+router.post("/placeorder", routerHeaders, isAuthenticated, placeOrder);
+router.post("/placeorderonline", routerHeaders, isAuthenticated, placeOrder);
+router.post("/paymentverification", routerHeaders, isAuthenticated, paymentVerification);
+router.get("/myorders", routerHeaders, isAuthenticated, getOrders);
+router.get("/order/:id", routerHeaders, isAuthenticated, getOrderDetails);
+router.get("/admin/orders", routerHeaders, isAuthenticated, authorizeAdmin, getAdminOrders);
+router.get("/admin/order/:id", routerHeaders, isAuthenticated, authorizeAdmin, processOrders);
 
 
 export default router;
--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,12 @@ import { authorizeAdmin, isAuthenticated } from '../middleware/auth.js';
 
 const router = express.Router();
 
+export const routerHeaders = (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://btechburgerwala-tau.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+};
 
 
 router.get("/auth/google", passport.authenticate("google", {
@@ -17,10 +23,10 @@ router.get("/google/callback", passport.authenticate("google", {
 }));
 
 router.post("/contact", contactDetails);
-router.get("/profile", isAuthenticated, myProfile);
+router.get("/profile", routerHeaders, isAuthenticated, myProfile);
 router.get("/logout", logout);
-router.get("/admin/users", isAuthenticated, authorizeAdmin, getAdminUsers);
-router.get("/admin/stats", isAuthenticated, authorizeAdmin, getAdminStats);
+router.get("/admin/users", routerHeaders, isAuthenticated, authorizeAdmin, getAdminUsers);
+router.get("/admin/stats", routerHeaders, isAuthenticated, authorizeAdmin, getAdminStats);
 
 
 export default router;
