69438c15cf3afd32d05739587a6d45372602618f
TusharV88 <66867543+TusharV88@users.noreply.github.com>
2022-11-27 19:43:39 +0530

    Initial Commit

--- routes/order.js
+++ routes/order.js
@@ -1,17 +1,16 @@
 import express from 'express';
 import { getAdminOrders, getOrderDetails, getOrders, paymentVerification, placeOrder, processOrders } from '../controllers/order.js';
 import { authorizeAdmin, isAuthenticated } from '../middleware/auth.js';
-import { routeHeaders } from '../routes/user.js';
 
 const router = express.Router();
 
-router.post("/placeorder", routeHeaders, isAuthenticated, placeOrder);
-router.post("/placeorderonline", routeHeaders, isAuthenticated, placeOrder);
-router.post("/paymentverification", routeHeaders, isAuthenticated, paymentVerification);
-router.get("/myorders", routeHeaders, isAuthenticated, getOrders);
-router.get("/order/:id", routeHeaders, isAuthenticated, getOrderDetails);
-router.get("/admin/orders", routeHeaders, isAuthenticated, authorizeAdmin, getAdminOrders);
-router.get("/admin/order/:id", routeHeaders, isAuthenticated, authorizeAdmin, processOrders);
+router.post("/placeorder", isAuthenticated, placeOrder);
+router.post("/placeorderonline", isAuthenticated, placeOrder);
+router.post("/paymentverification", isAuthenticated, paymentVerification);
+router.get("/myorders", isAuthenticated, getOrders);
+router.get("/order/:id", isAuthenticated, getOrderDetails);
+router.get("/admin/orders", isAuthenticated, authorizeAdmin, getAdminOrders);
+router.get("/admin/order/:id", isAuthenticated, authorizeAdmin, processOrders);
 
 
 export default router;
--- routes/user.js
+++ routes/user.js
@@ -6,14 +6,6 @@ import { authorizeAdmin, isAuthenticated } from '../middleware/auth.js';
 
 const router = express.Router();
 
-export const routeHeaders = (req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-};
 
 
 router.get("/auth/google", passport.authenticate("google", {
@@ -24,11 +16,11 @@ router.get("/google/callback", passport.authenticate("google", {
     successRedirect: "http://localhost:3000",
 }));
 
-router.post("/contact", routeHeaders, contactDetails);
-router.get("/profile", routeHeaders, isAuthenticated, myProfile);
-router.get("/logout", routeHeaders, logout);
-router.get("/admin/users", routeHeaders, isAuthenticated, authorizeAdmin, getAdminUsers);
-router.get("/admin/stats", routeHeaders, isAuthenticated, authorizeAdmin, getAdminStats);
+router.post("/contact", contactDetails);
+router.get("/profile", isAuthenticated, myProfile);
+router.get("/logout", logout);
+router.get("/admin/users", isAuthenticated, authorizeAdmin, getAdminUsers);
+router.get("/admin/stats", isAuthenticated, authorizeAdmin, getAdminStats);
 
 
 export default router;
