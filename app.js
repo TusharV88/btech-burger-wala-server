1d0eda87b19ad45d3b43774d274195d43dec2f22
TusharV88 <66867543+TusharV88@users.noreply.github.com>
2022-12-06 14:00:15 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ dotenv.config({ path: './config/.env' });
 // Middlewares
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', "https://btech-burger-wala.onrender.com");
+    res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     next();
