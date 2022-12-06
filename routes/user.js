55e000bc3f5ebf92482d3fb11b8e61083fa8c32e
TusharV88 <66867543+TusharV88@users.noreply.github.com>
2022-12-06 20:21:04 +0530

    Initial Commit

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,7 @@ import { authorizeAdmin, isAuthenticated } from '../middleware/auth.js';
 const router = express.Router();
 
 
-router.get("/auth/google", passport.authenticate("google", {
+router.get("/auth/google", loginToken, passport.authenticate("google", {
     scope: ["profile"],
 }));
 
@@ -16,7 +16,6 @@ router.get("/google/callback", passport.authenticate("google", {
 }));
 
 
-router.get("/", loginToken);
 router.post("/contact", contactDetails);
 router.get("/profile", isAuthenticated, myProfile);
 router.get("/logout", logout);
