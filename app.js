2e4bd58b428fcb2befd915a86d77afd1da6149c5
TusharV88 <66867543+TusharV88@users.noreply.github.com>
2022-12-07 13:14:08 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ app.use(session({
         sameSite: "none",
     },
 }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://btech-burger-wala.onrender.com");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 app.use(cookieParser());
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
