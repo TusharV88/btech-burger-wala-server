5c8cf6e63ae9319fc4e2d365364dc773d5ffb95b
TusharV88 <66867543+TusharV88@users.noreply.github.com>
2022-12-07 17:20:49 +0530

    Initial Commit

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,7 @@ router.get("/auth/google", passport.authenticate("google", {
     scope: ["profile"],
 }));
 
-router.get("/google/callback", passport.authenticate("google"), loginToken);
+router.get("/google/callback", passport.authenticate("google"), loginToken, (req, res) => { res.redirect("https://btech-burger-wala.onrender.com"); });
 
 router.post("/contact", contactDetails);
 router.get("/profile", isAuthenticated, myProfile);
