fee55ad22f537a751dcfd0fb4c38bdfe11e3242d
TusharV88 <66867543+TusharV88@users.noreply.github.com>
2022-12-09 20:29:34 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -17,13 +17,20 @@ app.set("trust proxy", 1);
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
         sameSite: "none",
         secure: true,
         httpOnly: false
     }
 }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://btech-burger-wala.onrender.com");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(cookieParser());
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
