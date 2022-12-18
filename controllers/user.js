58341261bcf2e7d39f6bdfb2685de58151d611b7
TusharV88 <66867543+TusharV88@users.noreply.github.com>
2022-12-18 21:28:25 +0530

    Fixed

--- controllers/user.js
+++ controllers/user.js
@@ -19,6 +19,7 @@ export const register = async (req, res) => {
             });
         }
 
+
         const myCloud = await cloudinary.v2.uploader.upload(avatar, { folder: "avatars" });
 
         user = await User.create({
